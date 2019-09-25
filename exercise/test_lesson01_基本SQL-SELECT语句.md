36f2575886ea980c8b28fccbbb29652ada6b3bff
cucker <hanxiao2100@qq.com>
2019-09-25 18:30:06 +0800

    add sql/条件查询.sql, update SQL语言.md

---
+++ exercise/test_lesson01_基本SQL-SELECT语句.md
@@ -0,0 +1,54 @@
+test_lesson01 基本SQL-SELECT语句
+==
+
+
+```
+# 练习select语句
+
+-- 1.下面的语句是否可以执行成功
+SELECT last_name , job_id , salary AS sal
+FROM employees;
+
+# 答案：可以
+
+-- 2.下面的语句是否可以执行成功
+SELECT * FROM employees;
+
+# 答案：可以
+
+-- 3.找出下面语句中的错误
+SELECT employee_id , last_name，
+salary * 12 “ANNUAL SALARY”
+FROM employees;
+
+# 答案：，-->,
+# “ANNUAL SALARY” --> "ANNUAL SALARY"
+
+-- 4.显示表 departments 的结构，并查询其中的全部数据
+DESC departments;
+SELECT * FROM departments;
+
+-- 5.显示出表 employees 中的全部 job_id（不能重复）
+SELECT DISTINCT job_id FROM employees;
+
+-- 6.显示出表 employees 的全部列，各个列之间用逗号连接，列头显示成 OUT_PUT
+SELECT
+    CONCAT (
+    `employee_id`, '_',
+    `first_name`, '_',
+    `last_name`, '_',
+    `email`, '_',
+    `phone_number`, '_',
+    `job_id`, '_',
+    `salary`, '_',
+    IFNULL(commission_pct, 0), '_',
+    `manager_id`, '_',
+    `department_id`, '_',
+    `hiredate`, '_'
+    ) AS "OUT_PUT"
+FROM
+    employees;
+
+
+
+```
---
+++ sql/条件查询.sql
@@ -0,0 +1,148 @@
+# 条件查询
+-- 语法
+ /*
+select 查询列表
+from 表名
+where 筛选条件;
+
+
+分类
+* 按条件表达式筛选
+比较运算符
+> < = <> != >= <=
+
+* 按逻辑表达式筛选
+逻辑运算符
+AND OR NOT
+&& || !
+
+* 模糊查询
+like
+between A and B
+in
+is null
+is not null
+
+*/
+
+
+-- 按条件表达式筛选
+# 示例：查询工资>12000的员工信息
+SELECT
+    *
+FROM
+    employees
+WHERE salary > 12000;
+
+# 案例2：查询部门编号不等于90号的员工名和部门编号
+SELECT last_name, department_id
+FROM employees
+WHERE department_id <> 90;
+
+
+SELECT last_name, department_id
+FROM employees
+WHERE department_id != 90;
+
+
+-- 按逻辑表达式筛选
+# 案例1：查询工资在10000到20000之间的员工名、工资以及奖金
+SELECT
+    last_name, salary, commission_pct
+FROM
+    employees
+WHERE salary >= 10000
+    AND salary <= 20000;
+    
+# 案例2：查询部门编号不是在90到110之间，或者工资高于15000的员工信息
+SELECT * 
+FROM employees
+WHERE NOT (department_id >= 90 AND department_id <= 110)
+    OR salary > 15000;
+
+
+-- 模糊查询
+ /*
+like
+
+betwenn A AND B
+in(set)
+is null
+is not null
+
+*/
+-- like '匹配模式'
+ /*
+一般和通配符搭配使用
+通配符：
+%：0个或多个任意字符
+_: 1个任意字符
+*/
+
+# 案例1：查询员工名中包含字符a的员工信息
+SELECT * 
+FROM employees
+WHERE first_name LIKE '%a%';
+
+
+# 案例2：查询员工名中第三个字符为e，第五个字符为a的员工名和工资
+SELECT first_name 名, CONCAT(first_name, ' ',last_name) 姓名,salary
+FROM employees
+WHERE first_name LIKE '__e_a%';
+
+# 案例3：查询员工姓中第二个字符为_的员工名
+-- ESCAPE '标识符' 显式指定转义，建议使用这种
+SELECT last_name 
+FROM employees
+WHERE last_name LIKE '_$_%' ESCAPE '$'; -- 指定$右边第一个字符为转义的，$可以用其他字符来标识
+
+
+SELECT last_name 
+FROM employees
+WHERE last_name LIKE '_\_%'; -- 这种转义方式也可以
+
+
+-- between A and B
+/*
+在[A, B]闭区间
+A,B都为数值类型
+*/
+# 案例1：查询员工编号在100到120之间的员工信息
+SELECT * 
+FROM employees
+WHERE employee_id >= 100 AND employee_id <= 120;
+
+-- 
+
+SELECT * 
+FROM employees
+WHERE employee_id BETWEEN 100 AND 120;
+
+
+-- IN(set)
+/*
+判断某字段的值是否在集合set中
+注意：
+set表示方式，ele1, ele2,...
+* set集合中的元素类型必须一致或兼容
+* set集合中的元素不支持通配符
+* set里的元素建议不重复
+
+*/
+
+# 案例：查询员工的工种编号是 IT_PROG、AD_VP、AD_PRES中的员工名和工种编号
+SELECT job_id, first_name
+FROM employees
+WHERE job_id = 'IT_PROG'
+    OR job_id = 'AD_VP'
+    OR job_id = 'AD_PRES';
+
+--
+SELECT job_id, first_name
+FROM employees
+WHERE job_id IN('IT_PROG', 'AD_VP', 'AD_PRES');
+
+
+
+
+

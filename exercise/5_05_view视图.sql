dbc138156e38bf342be54942a512b1a2f87ca54b
cucker <hanxiao2100@qq.com>
2019-10-13 23:43:09 +0800

    update exercise/5_05_view视图.sql

--- exercise/5_05_view视图.sql
+++ exercise/5_05_view视图.sql
@@ -119,7 +119,7 @@ ON myv3.department_id = d.department_id;
 
 
 
--- 视图的修改
+-- 修改视图的sql语句
 -- 
 /*
 ## 语法1
@@ -136,5 +136,101 @@ as
 ;
 
 */
+
+DESC myv3;
+
+SHOW CREATE VIEW myv3;
+
+-- 方式1
+CREATE OR REPLACE VIEW myv3
+AS
+SELECT AVG(salary), job_id
+FROM employees
+GROUP BY job_id;
+
+SHOW CREATE VIEW myv3;
+
+
+-- 方式2
+ALTER VIEW myv3
+AS 
+SELECT * FROM employees;
+
+SELECT * FROM myv3;
+
+
+-- 查看视图
+-- 
+/*
+# 语法
+desc 视图名;
+
+show create view 视图名;
+
+-- 查看本库所有已建的视图
+SHOW TABLE STATUS WHERE COMMENT='view';
+
+*/
+DESC myv3;
+
+SHOW CREATE VIEW myv3;
+
+SHOW TABLE STATUS WHERE COMMENT='view';
+
+
+-- 删除视图
+-- 
+/*
+## 语法
+drop view 视图名1 [, 视图名2, ...];
+
+需要有相应的权限才能执行
+*/
+
+DROP VIEW myv2, myv3;
+
+SHOW TABLE STATUS WHERE COMMENT='view';
+
+
+-- 视图虚拟表数据可更新情况
+-- 
+
+USE myemployees;
+
+CREATE OR REPLACE VIEW myv1
+AS
+SELECT last_name, email
+FROM employees;
+
+
+# 插入数据
+SELECT * FROM myv1;
+
+INSERT INTO myv1 VALUES ('张飞', 'zhangfei@qq.com');
+
+SELECT * FROM myv1; -- 已经添加
+
+SELECT * FROM employees; -- 表中已经添加插入的数据，其他字段为null。实际上是对表的更新
+
+# 修改表数据
+UPDATE myv1 SET email = 'zf@qq.com' WHERE last_name = '张飞';
+
+# 删除表记录
+DELETE FROM myv1 WHERE last_name = '张飞'; 
+
+
+-- 具备以下特点的视图不可更行(增删改)
+-- 
+
+-- ①包含下列关键字的sql语句:
+-- 分组函数、distinct、group by、having、 union、union all
+
+-- ②常量视图
+
+-- ③select中包含字查询
+
+-- ④
+
+-- ⑤
 
 

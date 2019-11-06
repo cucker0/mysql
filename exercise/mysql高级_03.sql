cd93df5e93285e1afe4755f1139fd7528cda4ebb
cucker <hanxiao2100@qq.com>
2019-11-06 21:59:45 +0800

    update mysql高级_03.sql

--- exercise/mysql高级_03.sql
+++ exercise/mysql高级_03.sql
@@ -57,14 +57,15 @@ EXISTS子句理解：
 -- 
 CREATE TABLE taba (
     age INT,
-    birth TIMESTAMP NOT NULL
+    birth TIMESTAMP NOT NULL,
+    `comment` VARCHAR(255)
 );
 
 
 INSERT INTO taba (age, birth) VALUES
-(22, NOW()),
-(23, NOW()),
-(24, NOW());
+(22, NOW(), '22岁'),
+(23, NOW(), '23岁'),
+(24, NOW(), '24岁');
 
 -- 建立索引
 CREATE INDEX idx_taba_age_birth ON taba (age, birth);
@@ -78,26 +79,26 @@ WHERE age = 20;
 
 -- 1_1
 EXPLAIN
-SELECT * FROM taba 
+SELECT age, birth FROM taba 
 WHERE age > 20
 ORDER BY age;
 
 
 -- 1_2
 EXPLAIN
-SELECT * FROM taba
+SELECT age, birth FROM taba
 WHERE age > 20
 ORDER BY age, birth;
 
 -- 1_3
 EXPLAIN
-SELECT * FROM taba
+SELECT age, birth FROM taba
 WHERE age > 20
 ORDER BY birth;
 
 -- 1_4
 EXPLAIN
-SELECT * FROM taba
+SELECT age, birth FROM taba
 WHERE age > 20
 ORDER BY birth, age;
 
@@ -105,35 +106,77 @@ ORDER BY birth, age;
 
 -- 2_1
 EXPLAIN
-SELECT * FROM taba 
+SELECT age, birth FROM taba 
 ORDER BY birth;
 
 -- 2_2
 EXPLAIN
-SELECT * FROM taba
+SELECT age, birth FROM taba
 WHERE birth > '2019-11-06 00:00:00'
 ORDER BY birth;
 
 -- 2_3
 EXPLAIN
-SELECT * FROM taba
+SELECT age, birth FROM taba
 WHERE birth > '2019-11-06 00:00:00'
 ORDER BY age;
+/*
 -- 与1_3的比较
+这里为什么只用到了index排序，而没有产生filesort排序呢？
+这主要是因为使用到了覆盖索引，因为idx_taba_age_birth (age, birth) 而select * 即为 select age, birth
+*/
 
 -- 2_4
 EXPLAIN
-SELECT * FROM taba
+SELECT age, birth FROM taba
 ORDER BY age ASC, birth DESC;
 
+-- 2_5
+EXPLAIN
+SELECT age, birth FROM taba
+ORDER BY age DESC, birth DESC;
+/*
+使用索引排序，没有产生filesort
+这是因为ORDER BY片排序的字段排序方向一致
+
+用到了索引age,birth，
+Extra为Backward index scan; Using index
+*/
 
 
 
+-- 为排序使用索引
+-- 
+/*
+mysql有两种排序方式：filesort、index
 
 
+mysql能为排序和查询使用相同的索引
+
+index a_b_c (a, b, c)
+*/
 
 
+-- order by符合最佳左前缀法，则能用到index排序，如下：
+... ORDER BY a;
+... ORDER BY a, b;
+... ORDER BY a, b, c;
+... ORDER BY a, DESC, b DESC, c DESC; -- 排序字段的排序方向都一致，所以能用到index排序
 
+
+-- 如果where使用索引的最左前缀定义为常量，则order by能使用索引
+... WHERE a = const ORDER BY b, c;
+... WHERE a = const AND b = const ORDER BY c;
+... WHERE a = const ORDER BY b, c;
+... WHERE a = const AND b > const ORDER BY b, c;
+
+
+-- 不能使用索引进行排序
+... ORDER BY a ASC, b DESC, c DESC  -- order by排序字段的排序方向不一致
+... WHERE g = const ORDER BY b, c;  -- 丢失索引a字段
+... WHERE a = const ORDER BY c;  -- 丢失索引b字段
+... WHERE a = const ORDER BY a, d;  -- 字段d不是索引字段
+... WHERE a IN (...) ORDER BY b, c;  -- 对于排序来说，多个相等条件也是范围，当然覆盖索引除外
 
 
 

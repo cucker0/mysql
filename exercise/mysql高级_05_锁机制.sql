d280917ec4380c687bbc53503471afdfc2192bc8
cucker <hanxiao2100@qq.com>
2019-11-10 00:10:57 +0800

    update mysql高级.锁机制.md

--- exercise/mysql高级_05_锁机制.sql
+++ exercise/mysql高级_05_锁机制.sql
@@ -1,7 +1,10 @@
 -- mysql高级_05_锁机制
 
 
--- 表
+-- 表锁
+-- 
+
+-- 表结构
 CREATE TABLE mylock (
     id INT PRIMARY KEY AUTO_INCREMENT,
     `name` VARCHAR(20)
@@ -29,3 +32,44 @@ In_use：
 */
 
 SELECT * FROM staffs;
+
+
+
+SHOW STATUS LIKE 'table%';
+
+
+
+-- 行锁
+-- 
+
+
+-- 表结构
+CREATE TABLE test_innodb_lock (
+    a INT,
+    b VARCHAR(16)
+) ENGINE = INNODB;
+
+
+INSERT INTO test_innodb_lock VALUES
+(1, 'b2'),
+(3, '3'),
+(4, '4000'),
+(5, '5000'),
+(6, '6000'),
+(7, '7000'),
+(8, '8000'),
+(9, '9000'),
+(1, 'b1');
+
+SELECT * FROM test_innodb_lock;
+
+-- 创建索引
+CREATE INDEX idx_test_innodb_lock__a ON test_innodb_lock (a);
+
+CREATE INDEX idx_test_innodb_lock__b ON test_innodb_lock (b);
+
+
+-- 测试1
+
+
+
